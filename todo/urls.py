768983ed226dff5e785a28023c461411cb712936
rexsybimatw <rexsy.bimq12@gmail.com>
2025-11-28 09:54:10 +0700

    added small feature for updating exsting data

--- todo/urls.py
+++ todo/urls.py
@@ -7,8 +7,18 @@ urlpatterns = [
     path("get/<int:pk>", views.get_todo, name=views.get_todo.__name__),
     path("get_htmx/<int:pk>", views.get_todo_htmx, name=views.get_todo_htmx.__name__),
     path("update/<int:pk>", views.update_todo, name=views.update_todo.__name__),
+    path(
+        "update_htmx/<int:pk>",
+        views.update_todo_htmx,
+        name=views.update_todo_htmx.__name__,
+    ),
     path("create/", views.create_todo, name=views.create_todo.__name__),
     path("create_htmx/", views.create_todo_htmx, name=views.create_todo_htmx.__name__),
+    path(
+        "create_htmx_v2/",
+        views.create_todo_htmx_v2,
+        name=views.create_todo_htmx_v2.__name__,
+    ),
     path("delete/", views.delete_element, name=views.delete_element.__name__),
     path("delete/<int:pk>", views.delete_todo, name=views.delete_todo.__name__),
 ]
--- todo/views.py
+++ todo/views.py
@@ -8,7 +8,7 @@ from .models import Todo
 # Create your views here.
 def get_all_todos(request: HttpRequest):
     todos = Todo.objects.all()
-    return render(request, "todo/all_todos.html", {"todos": todos})
+    return render(request, "todo/all_todos.html", {"todos": todos, "count": len(todos)})
 
 
 def get_todo(request: HttpRequest, pk: int):
@@ -57,7 +57,9 @@ def delete_element(request: HttpRequest):
 
 def get_all_todos_htmx(request: HttpRequest):
     todos = Todo.objects.all()
-    return render(request, "todo/all_todos_htmx.html", {"todos": todos})
+    return render(
+        request, "todo/all_todos_htmx.html", {"todos": todos, "count": len(todos)}
+    )
 
 
 def get_todo_htmx(request: HttpRequest, pk: int):
@@ -69,9 +71,47 @@ def create_todo_htmx(request: HttpRequest):
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Todo created successfully")
-            return redirect("get_all_todos")  # adjust to your URL name
+            todo = form.save()
+            return render(request, "todo/new_todo_component.html", {"todo": todo})
     else:
         form = TodoForm()
     return render(request, "todo/create_todo_htmx.html", {"form": form})
+
+
+def create_todo_htmx_v2(request: HttpRequest):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save()
+            count = len(Todo.objects.all())
+            print(count)
+            return render(
+                request,
+                "todo/new_todo_component_v2.html",
+                {"todo": todo, "count": count},
+            )
+    else:
+        form = TodoForm()
+    return render(request, "todo/create_todo_htmx_v2.html", {"form": form})
+
+
+def update_todo_htmx(request: HttpRequest, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save()
+            return render(
+                request, "todo/update_todo_htmx_component.html", {"todo": todo}
+            )
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, "todo/update_todo_htmx.html", {"form": form, "todo": todo})
+
+
+def delete_todo_htmx(request: HttpRequest, pk):
+    """WIP"""
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "DELETE":
+        todo.delete()
+    return render(request, "todo/update_todo_htmx.html", {})
